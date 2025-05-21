5e371c3ad97a3d3dac878984a1d540e851991492
John Hennessy <johnthennessy@yahoo.co.uk>
2025-05-21 10:55:17 +0100

    Working on Linux

---
+++ outdoors/wp-content/uploads2/sucuri/sucuri-auditqueue.php
@@ -0,0 +1,7 @@
+<?php
+// datastore=auditqueue;
+// created_on=1582282315;
+// updated_on=1582282315;
+exit(0);
+?>
+1582282315_0956:"Debug: hill_wanderer, 85.211.178.93; Sucuri plugin has been uninstalled"
